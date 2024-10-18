f9a5e4cae29111fac43408a4bc7a966a98cd9474
MaxMartinez2006 <memartionez2004@gmial.com>
2024-10-17 21:24:01 -0600

    primer commit

---
+++ facturacion1/src/main/java/com/tuempresa/facturacion1/modelo/Producto.java
@@ -0,0 +1,20 @@
+package com.tuempresa.facturacion1.modelo;
+
+import javax.persistence.*;
+
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+
+
+public class Producto {
+	@Id @Column(length = 9)
+	int numero;
+	
+	@Column(length = 50) @Required
+	String descripcion;
+
+}
